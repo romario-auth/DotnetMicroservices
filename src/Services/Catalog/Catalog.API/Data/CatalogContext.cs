5dda5ea328a3c511463909713feeab3f375c2130
rromario <romario.damasceno@zukk.com.br>
2022-04-17 03:02:53 -0300

    [Ms Catalog] Creating entities Product and configured Mongo conection

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
