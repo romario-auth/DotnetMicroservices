aa02b5ebf88e430e3fa2faadf922d14c0b4cb95b
Romario de Melo Damasceno <romario.damasceno@consulting-for.edenred.com>
2022-07-31 01:01:02 -0300

    [Discount.Grpc] Developing DiscountService to expose GRPC and Implement AutoMapper

---
+++ src/Services/Discount/Discount.Grpc/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _discountrepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository discountrepository, IMapper mapper, ILogger<DiscountService> logger)
+        {
+            _discountrepository = discountrepository ?? throw new ArgumentNullException(nameof(discountrepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            Coupon coupon = await _discountrepository.GetDiscount(request.ProductName);
+
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} is not found."));
+            }
+
+            _logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+
+            CouponModel couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+        }
+    }
+}
