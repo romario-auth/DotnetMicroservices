27f89cfe8aa0e11ae8cc103db419ca87a716299f
Romario de Melo Damasceno <romario.damasceno@consulting-for.edenred.com>
2022-10-16 15:33:30 -0300

    [Ms Order] Developing Ordering.Application Layer-Application Features - GetOrdersListQuery

--- src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -1,8 +1,14 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mapping
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
     }
 }
